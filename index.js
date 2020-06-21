069d77f4f7d331e0acc00a6cbbf3c35227424993
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-21 22:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function happyHolidaysTo(string){
 //Write a function named happyCustomHolidayTo. This function should accept two parameters, the holiday you want to wish them well for, and the name of the person you're wishing well. Order of parameters matters, so make sure to first pass in the holiday and then the name. This function should return the string `Happy ${holiday}, ${name}!`
 function happyCustomHolidayTo(string1,string2){
   return `Happy ${string1}, ${string2}!`;
+  // i thought function inputs were types, not variables.
 }
 
 //Write a function named holidayCountdown. This function should accept two parameters, the holiday name and number of days until that holiday. The function should return the string `It's ${days} days until ${holiday}!`. Note that although days is used first when constructing the returned string, holidayCountdown should take in the holiday name first, then the days until that holiday.
